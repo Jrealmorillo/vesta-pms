ab672b96b2424bc78ba99b89e3720550acdbea6a
Jose Real <jrealmorillo@gmail.com>
2025-03-19 17:58:02 +0100

    Moved middlewares folder to /src

--- backend/src/routes/usuarioRoutes.js
+++ backend/src/routes/usuarioRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const usuarioController = require("../controllers/usuarioController");
-const authMiddleware = require("../../middlewares/authMiddleware");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 
 const router = express.Router();
